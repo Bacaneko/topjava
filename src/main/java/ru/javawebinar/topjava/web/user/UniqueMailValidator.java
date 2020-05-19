dc829bb963e776c264e4d8f638fc5231945cdbff
Bacaneco <proba1bust1@mail.ru>
2020-05-19 03:18:23 +0300

    11_05_HW10_binder_validation

---
+++ src/main/java/ru/javawebinar/topjava/web/user/UniqueMailValidator.java
@@ -0,0 +1,35 @@
+package ru.javawebinar.topjava.web.user;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.javawebinar.topjava.HasIdAndEmail;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.web.ExceptionInfoHandler;
+
+
+@Component
+public class UniqueMailValidator implements Validator {
+
+    @Qualifier("dataJpaUserRepository")
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return HasIdAndEmail.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        HasIdAndEmail user = ((HasIdAndEmail) target);
+        User dbUser = repository.getByEmail(user.getEmail().toLowerCase());
+        if (dbUser != null && !dbUser.getId().equals(user.getId())) {
+            errors.rejectValue("email", ExceptionInfoHandler.EXCEPTION_DUPLICATE_EMAIL);
+        }
+    }
+}
